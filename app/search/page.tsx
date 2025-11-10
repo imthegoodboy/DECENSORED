b6cf754f09f12a579cfc01558cc801b8784c1369
Nikku <nikku696969@gmail.com>
2025-11-10 09:11:38 +0530

    updating

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { useSearchParams } from 'next/navigation';
+
+export const dynamic = 'force-dynamic';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Navbar from '@/components/Navbar';
