ab08c213e3a1b46c1d9030e43bb13e4e6283231e
Nikku <nikku696969@gmail.com>
2025-11-10 09:33:16 +0530

    updating

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
   images: {
     domains: ['ipfs.io', 'gateway.pinata.cloud', 'arweave.net', 'ui-avatars.com'],
     unoptimized: true,
